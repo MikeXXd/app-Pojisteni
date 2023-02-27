c7fa4722baeb8db22cd0e483ccf07fb2dfb61048
Mike <mike3d@seznam.cz>
2023-02-27 18:58:38 +0100

    in progress delete-buttons

--- js/index/Kartoteka.js
+++ js/index/Kartoteka.js
@@ -12,20 +12,25 @@ class Kartoteka {
         this.telefonInput = document.getElementById("input-tel");
         this.ulozitButton = document.getElementById("button-ulozit");
         this.vypisPojistence = document.getElementById("pojistenci-seznam");
+        
+
+
+        /* https://www.itnetwork.cz/javascript/oop/oop-diar-v-javascriptu-formatovani-a-mazani-zaznamu#:~:text=Mo%C5%BEn%C3%A1%20by%20v%C3%A1s,konkr%C3%A9tn%C3%AD%20polo%C5%BEku%20p%C5%99ed%C3%A1me. */
 
         this.obsluhaUdalosti();
+
 
     }
 
     obsluhaUdalosti() {
         this.ulozitButton.onclick = () => {
-            if (this.jmenoInput.value.length < 2) {
+            if (this.jmenoInput.value.length < 1) {
                 alert("Prosím zadej jméno v délce minimálně 2 znaků");
             }
-            if (this.prijmeniInput.value.length < 2) {
+            if (this.prijmeniInput.value.length < 1) {
                 alert("Prosím zadej príjmení v délce minimálně 2 znaků");
             }
-            if (this.telefonInput.value.length < 9) {
+            if (this.telefonInput.value.length < 1) {
                 alert("Prosím zadej telefonni číslo v délce minimálně 9 číslic");
             }
             else {
@@ -37,8 +42,10 @@ class Kartoteka {
                 this.vekInput.value = '';
                 this.telefonInput.value = '';
                 this.vypisZaznamy();
+                
+            
             }
-        }
+        };
     }
 
     vypisZaznamy() {
@@ -49,8 +56,9 @@ class Kartoteka {
             let capJmeno = zapis.jmeno.replace(zapis.jmeno[0], zapis.jmeno[0].toUpperCase()); // Velké písmeno u jména
             let capPrijmeni = zapis.prijmeni.replace(zapis.prijmeni[0], zapis.prijmeni[0].toUpperCase()); // Velké písmeno u příjmení
             
-             elVypis.innerHTML += `<tr><td>${capJmeno} ${capPrijmeni}</td><td>${zapis.tel}</td><td>${this.ageCalculator(zapis.vek)}</td><td>${zapis.datumVstupu}</td></tr>`;  
+             elVypis.innerHTML += `<tr><td>${capJmeno} ${capPrijmeni}</td><td>${zapis.tel}</td><td>${this.ageCalculator(zapis.vek)}</td><td>${zapis.datumVstupu}</td><td><button data-index="${i}" class="myButton btn btn-outline-danger btn-sm">X</button></td></tr>`; 
         }
+
     }
 
     ageCalculator(datumNar) {
@@ -73,10 +81,9 @@ class Kartoteka {
             
             //display the calculated age
             return age
-        }
-    }
+        }}
 
-
+    
 }
     
 
--- js/oAplikaci.js
+++ js/oAplikaci.js
@@ -1,6 +1,14 @@
 
  const deleteAll = () => {
-    if (confirm("Opravdu chceš vše vymazat?")) {
-                localStorage.clear();
-            }
-  }
+    let confirmMessage = confirm("Opravdu chceš vše vymazat?");
+        if (confirmMessage) {
+            localStorage.clear();
+            window.location.reload();
+            alert("Data byly úspěšně vymazány");
+        } else {
+            window.location.reload();
+            alert("Zrušeno, data byly zachvány!");
+        }
+                
+}
+  
