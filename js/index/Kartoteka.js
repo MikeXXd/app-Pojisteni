e2f180bd6090f95ffcacf1955bbf3f708c6ae4cc
Mike <mike3d@seznam.cz>
2023-03-12 21:55:08 +0100

    sorting columns applied

--- js/index/Kartoteka.js
+++ js/index/Kartoteka.js
@@ -14,9 +14,11 @@ class Kartoteka {
         this.telefonInput = document.getElementById("input-tel");
         this.ulozitButton = document.getElementById("button-ulozit");
         this.vypisPojistence = document.getElementById("pojistenci-seznam");
-        this.aktivacniDltBtn = document.getElementById("activacni-dlt-btn");
+        this.aktivacniDltBtn = document.getElementById("activacni-dlt-btn"); // button pro aktivaci mazacích tlačítek u pojištěnců
         this.razeniJmenaBtn = document.getElementById("razeni-jmena-btn");
         this.razeniPrijmeniBtn = document.getElementById("razeni-prijmeni-btn");
+        this.razeniVekBtn = document.getElementById("razeni-vek-btn");
+        this.razeniDatumVstupuBtn = document.getElementById("razeni-datum-vstupu-btn");
         
         this.obsluhaUdalosti();
     }
@@ -31,8 +33,7 @@ class Kartoteka {
             }
             if (this.telefonInput.value.length < 1) {
                 alert("Zadej telefonni číslo");
-            }
-            else {
+            } else {
                 const pojistenec = new Pojistenec(this.jmenoInput.value, this.prijmeniInput.value, this.vekInput.value, this.telefonInput.value);
                 this.pojistenci.push(pojistenec); // na konec pole pojistenci se přidá nový objekt pojistenec
                 localStorage.setItem("pojistenci", JSON.stringify(this.pojistenci)); /* nově  */
@@ -70,32 +71,32 @@ class Kartoteka {
                 
             }
         )
-//nefukční varianta, querySelectorAll nechce vidět .dltPojistenceBtn, a getElementsByClassNode zase nejede kvůli forEach
-        // const dltBtn = document.querySelectorAll(".dltPojistenceBtn");
-        // console.log(dltBtn);
-        // dltBtn.forEach(btn => {
-        //     btn.addEventListener('click', () => {
-        // let index = parseInt(btn.getAttribute("data-index"));
-        // this.vymazZaznam(index);
-        //     })
-        // })
         this.razeniJmenaBtn.onclick = () => {
             this.seradSloupec("jmeno");   
         }
         this.razeniPrijmeniBtn.onclick = () => {
             this.seradSloupec("prijmeni");   
         }
+        this.razeniVekBtn.onclick = () => {
+            this.seradSloupec("vek");   
+        }
+        this.razeniDatumVstupuBtn.onclick = () => {
+            this.seradSloupec("datumVstupu");   
+        }
 
+        
     } /* KONEC obsluha udalosti------------------------------------------- */
 
     seradSloupec(sloupec) {
         let x = 1
         let y = -1
         if (this.indexPosladniSerazenySpoupec == sloupec){
-           x = -1
-           y = 1
-           this.indexPosladniSerazenySpoupec = null;
-        } else {this.indexPosladniSerazenySpoupec = sloupec}
+            x = -1
+            y = 1
+            this.indexPosladniSerazenySpoupec = null;
+        } else {
+            this.indexPosladniSerazenySpoupec = sloupec
+        }
         this.pojistenci.sort((b, a) => { 
                 const nameA = a[sloupec].toUpperCase(); // ignore upper and lowercase
                 const nameB = b[sloupec].toUpperCase(); // ignore upper and lowercase
@@ -107,26 +108,9 @@ class Kartoteka {
                 }
                 // names must be equal
                 return 0;
-                
         });
-        console.log(x);
         this.vypisZaznamy();
-
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
     vypisZaznamy() {
         let elVypis = this.vypisPojistence;
@@ -135,7 +119,7 @@ class Kartoteka {
             let zapis = this.pojistenci[i]
             let capJmeno = zapis.jmeno.replace(zapis.jmeno[0], zapis.jmeno[0].toUpperCase()); // Velké písmeno u jména
             let capPrijmeni = zapis.prijmeni.replace(zapis.prijmeni[0], zapis.prijmeni[0].toUpperCase()); // Velké písmeno u příjmení
-             elVypis.innerHTML += `<tr><td>${capPrijmeni} ${capJmeno}</td><td>${zapis.tel}</td><td>${this.ageCalculator(zapis.vek)}</td><td>${zapis.datumVstupu}</td><td><button data-index="${i}" ${this.aktivaceButtonuMazaniUPojistencu()}>X</button></td></tr>`; 
+            elVypis.innerHTML += `<tr><td>${capPrijmeni} ${capJmeno}</td><td>${zapis.tel}</td><td>${this.ageCalculator(zapis.vek)}</td><td>${zapis.datumVstupu}</td><td><button data-index="${i}" ${this.aktivaceButtonuMazaniUPojistencu()}>X</button></td></tr>`; 
         }
 
     }
@@ -146,66 +130,37 @@ class Kartoteka {
         if(userinput == null || userinput == '') {
           return "nezadán!"; 
         } else {
-            //výpočet rozdílů času v milisekundach
-            let mesiceRozdil = Date.now() - datNar.getTime();
+            let mesiceRozdil = Date.now() - datNar.getTime(); //výpočet rozdílů času v milisekundach
             // console.log("Date.now: " + Date.now() + " - datNar.getTime: " + datNar.getTime() + " = mesiceRozdil: " +  mesiceRozdil);
-            //formatování datumu
-            let age_dt = new Date(mesiceRozdil); 
+            let age_dt = new Date(mesiceRozdil); //formatování datumu
             // console.log(age_dt);
-            //extrakce roku 
-            let year = age_dt.getUTCFullYear();
+            let year = age_dt.getUTCFullYear();//extrakce roku
             // console.log(year);
-            //dopočet věku - zrozeni křemíku :-)
-            let age = Math.abs(year - 1970);
+            let age = Math.abs(year - 1970);//dopočet věku - zrozeni křemíku :-)
             return age
         }}
 
-        addGlobalEventListener(type, selector, callback) { // všeobecná funkce pro nastavení EventListeneru
-            document.addEventListener(type, e => {
-                if(e.target.matches(selector)) {
+    addGlobalEventListener(type, selector, callback) { // všeobecná funkce pro nastavení EventListeneru
+        document.addEventListener(type, e => {
+            if(e.target.matches(selector)) {
                     callback(e)
-                }
-            })
-        }
-
-        vymazZaznam(index) {
-            this.pojistenci.splice(index, 1);
-            localStorage.setItem("pojistenci", JSON.stringify(this.pojistenci));
-            this.vypisZaznamy();
-        }
-
-        aktivaceButtonuMazaniUPojistencu() { 
-            if (!this.statusMazani) {
-                return "class='dltPojistenceBtn btn btn-outline-secondary btn-sm' disabled role='button' aria-disabled='true'"
-            } else {
-                return "class='dltPojistenceBtn btn btn-outline-danger btn-sm dltBtnActive' role='button' aria-disabled='false'"
             }
+        })
+    }
 
+    vymazZaznam(index) {
+        this.pojistenci.splice(index, 1);
+        localStorage.setItem("pojistenci", JSON.stringify(this.pojistenci));
+        this.vypisZaznamy();
+    }
+
+    aktivaceButtonuMazaniUPojistencu() { 
+        if (!this.statusMazani) {
+             return "class='dltPojistenceBtn btn btn-outline-secondary btn-sm' disabled role='button' aria-disabled='true'"
+        } else {
+            return "class='dltPojistenceBtn btn btn-outline-danger btn-sm dltBtnActive' role='button' aria-disabled='false'"
         }
-
-        razeniPojistencuString(dle) {
-          
-            if (this.indexPosladniSerazenySpoupec == null || !dle) {
-                
-            
-
-            this.pojistenci.sort((a, b) => {
-                const nameA = a.jmeno.toUpperCase(); // ignore upper and lowercase
-                const nameB = b.jmeno.toUpperCase(); // ignore upper and lowercase
-                if (nameA < nameB) {
-                  return -1;
-                }
-                if (nameA > nameB) {
-                  return 1;
-                }
-                // names must be equal
-                return 0;
-              });
-            }
-              this.vypisZaznamy();
-              this.indexPosladniSerazenySpoupec = "jmeno"
-        }
-    
+    }
 }
     
 
